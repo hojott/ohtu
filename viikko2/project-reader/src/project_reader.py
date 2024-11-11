b17592f8117f85a9155b10dcd6874ed3680b4f7a
Sakari Marttinen <sakkenino@gmail.com>
2024-11-11 21:41:12 +0200

    Add project-reader

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,23 @@
+import tomllib
+from urllib import request
+from project import Project
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        data = tomllib.loads(content)
+        
+        # TODO: Check if exists
+        name = data["tool"]["poetry"]["name"]
+        description = data["tool"]["poetry"]["description"]
+        license = data["tool"]["poetry"]["license"]
+        authors = data["tool"]["poetry"]["authors"]
+        dependencies = data["tool"]["poetry"]["dependencies"].keys()
+        dev_dependencies = data["tool"]["poetry"]["group"]["dev"]["dependencies"].keys()
+
+        return Project(name, description, license, authors, dependencies, dev_dependencies)
